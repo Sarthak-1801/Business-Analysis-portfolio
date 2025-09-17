f0f1dc609c5553beee3ee55a1dc6d87040c9e7a6
Sarthak-1801 <madhursarthak18@gmail.com>
2025-09-17 11:33:37 +0530

    Add files via upload

---
+++ Retail Store data SQL and Power BI Analysis/data cleaning.sql
@@ -0,0 +1,41 @@
+-- Cleaning
+DELETE s1 FROM Sales s1
+JOIN Sales s2 
+ON s1.Order_ID = s2.Order_ID 
+AND s1.Product_ID = s2.Product_ID 
+AND s1.Order_Line > s2.Order_Line;
+
+ALTER TABLE Sales MODIFY COLUMN Order_Date DATE;
+ALTER TABLE Sales MODIFY COLUMN Ship_Date DATE;
+
+UPDATE Sales
+SET Sales = NULL
+WHERE Sales <= 0;
+
+UPDATE Customer
+SET Customer_Name = TRIM(Customer_Name);
+
+UPDATE Sales
+SET Discount = 0
+WHERE Discount < 0;
+UPDATE Sales
+SET Discount = 1
+WHERE Discount > 1;
+
+UPDATE Sales
+SET Ship_Mode = 'Standard Class'
+WHERE Ship_Mode LIKE '%Standard%';
+
+UPDATE Product
+SET Product_Name = TRIM(Product_Name);
+
+
+SELECT s.*
+FROM Sales s
+LEFT JOIN Customer c ON s.Customer_ID = c.Customer_ID
+WHERE c.Customer_ID IS NULL;
+
+SELECT s.*
+FROM Sales s
+LEFT JOIN Product p ON s.Product_ID = p.Product_ID
+WHERE p.Product_ID IS NULL;
---
+++ Retail Store data SQL and Power BI Analysis/data exploration.sql
@@ -0,0 +1,76 @@
+
+-- Top 10 customers by Sales value
+SELECT c.Customer_Name, SUM(s.Sales) AS Total_Sales
+FROM Sales s
+JOIN Customer c ON s.Customer_ID = c.Customer_ID
+GROUP BY c.Customer_Name
+ORDER BY Total_Sales DESC
+LIMIT 10;
+
+-- Categorywise Revenue and Profit
+SELECT p.Category, SUM(s.Sales) AS Revenue, SUM(s.Profit) AS Profit
+FROM Sales s
+JOIN Product p ON s.Product_ID = p.Product_ID
+GROUP BY p.Category
+ORDER BY Revenue DESC;
+
+-- Regionwise Sales
+SELECT c.Region, ROUND(SUM(s.Sales),2) AS Total_Sales
+FROM Sales s
+JOIN Customer c ON s.Customer_ID = c.Customer_ID
+GROUP BY c.Region
+ORDER BY Total_Sales DESC;
+
+-- Monthly Sales Trend
+SELECT DATE_FORMAT(Order_Date, '%Y-%m') AS Month, ROUND(SUM(Sales), 2) AS Monthly_Sales
+FROM Sales
+GROUP BY Month
+ORDER BY Month;
+
+-- Customer retention analysis
+with FirstPurchase AS (
+select Customer_ID, MIN(Order_Date) AS First_Order
+from Sales
+group by Customer_ID
+),
+Cohort as (
+select 
+f.Customer_ID,
+date_format(f.First_Order, '%Y-%m') as Cohort_Month,
+date_format(s.Order_Date, '%Y-%m') as Order_Month
+from FirstPurchase f
+join Sales s on f.Customer_ID = s.Customer_ID
+)
+select 
+Cohort_Month,
+Order_Month,
+count(distinct Customer_ID) as Active_Customers
+from Cohort
+group by Cohort_Month, Order_Month
+order by Cohort_Month, Order_Month;
+
+-- Top 5 categorically most profitable products
+SELECT Category, Product_ID, Product_Name, Total_Profit
+FROM (SELECT 
+p.Category,
+p.Product_ID,
+p.Product_Name,
+SUM(s.Profit) AS Total_Profit,
+ROW_NUMBER() OVER (PARTITION BY p.Category ORDER BY SUM(s.Profit) DESC) AS rnk
+FROM Sales s
+JOIN Product p ON s.Product_ID = p.Product_ID
+GROUP BY p.Category, p.Product_ID, p.Product_Name
+) ranked
+WHERE rnk <= 5
+ORDER BY Category, Total_Profit DESC;
+
+-- Profit across regions
+SELECT 
+c.Region,
+SUM(s.Sales) AS Total_Sales,
+SUM(s.Profit) AS Total_Profit,
+ROUND(SUM(s.Profit)/SUM(s.Sales)*100,2) AS Profit_Margin_Percent
+FROM Sales s
+JOIN Customer c ON s.Customer_ID = c.Customer_ID
+GROUP BY c.Region
+ORDER BY Profit_Margin_Percent DESC;
